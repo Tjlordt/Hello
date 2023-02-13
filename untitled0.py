0db9e824f22f10eac7e34da545c0c91b56e2fc84
Tjlordt <125294384+Tjlordt@users.noreply.github.com>
2023-02-13 18:18:41 +0000

    Fundamental week 1

---
+++ untitled0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  7 02:16:47 2023
+
+@author: tj
+"""
+
+
+# a comment
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read("countries_top10.csv")
+print(df)
